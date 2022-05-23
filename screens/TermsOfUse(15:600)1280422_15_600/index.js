054c58a985d2e30a5ef4ac548efad5d09b3c3414
Crowdbotics <team@crowdbotics.com>
2022-05-23 10:52:55 +0000

    Update components

---
+++ screens/TermsOfUse(15:600)1280422_15_600/index.js
@@ -0,0 +1,314 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_15_601}>
+        <View style={styles.View_I15_601_228_176}>
+          <Text style={styles.Text_I15_601_228_176}>Terms of Use</Text>
+        </View>
+        <View style={styles.View_I15_601_228_177}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/ba30/b758/2150ad9262068f872f9328883a5e36c8"
+            }}
+            style={styles.ImageBackground_I15_601_228_177_154_460}
+          />
+        </View>
+      </View>
+      <View style={styles.View_15_602}>
+        <Text style={styles.Text_15_602}>
+          Last Modified: August 13, 2020 Acceptance of the Terms of Use Welcome
+          to the website of Circa Labs Inc (“Company”, “we”, “our” or “us”). The
+          following Terms of Use, togetherh with any documents they expressly
+          incorporate by reference (collectively these “Terms of Use”), govern
+          your access to and use of the Circa app, including any content,
+          functionality and service offered on or through wearecirca.com (the
+          “Website”). Please read these Terms of Use carefully before you start
+          to use the Website. By using the Website, you accept and agree to be
+          bound and abide by these Terms of Use, our Privacy Policy (found at
+          wearecirca.com/privacy), and our Copyright Policy
+          (wearecirca.com/copyright), incorporated herein by reference. If you
+          do not want to agree to these Terms of Use, the Privacy Policy, or the
+          Copyright Policy, you must not access or use the Website. This Website
+          is offered and available to users who are 18 years of age or older and
+          reside in the United States or any of its terriroties or possessions.
+          By using this Website, you represent blah blah blah blah blah blah
+          blah blah
+        </Text>
+      </View>
+      <View style={styles.View_15_603}>
+        <View style={styles.View_I15_603_137_706}>
+          <View style={styles.View_I15_603_137_707} />
+          <ImageBackground
+            source={{
+              uri:
+                "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/525b/1182/3379c8a6088a242fe4f03785d2d970eb"
+            }}
+            style={styles.ImageBackground_I15_603_137_708}
+          />
+          <View style={styles.View_I15_603_137_709} />
+        </View>
+        <ImageBackground
+          source={{
+            uri:
+              "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/1b81/843f/2074ecb78b933ae2dfe895e1affaec7a"
+          }}
+          style={styles.ImageBackground_I15_603_137_710}
+        />
+        <ImageBackground
+          source={{
+            uri:
+              "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/3c15/8f2b/0490361ab74ccb4989d0ed026428146e"
+          }}
+          style={styles.ImageBackground_I15_603_137_714}
+        />
+        <View style={styles.View_I15_603_137_719}>
+          <View style={styles.View_I15_603_137_720}>
+            <Text style={styles.Text_I15_603_137_720}>9:41</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.View_15_604}>
+        <View style={styles.View_I15_604_137_722} />
+      </View>
+    </ScrollView>
+  )
+}
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("111%") },
+  View_15_601: {
+    width: wp("100%"),
+    height: hp("7%"),
+    top: hp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    overflow: "hidden"
+  },
+  View_I15_601_228_176: {
+    flexGrow: 1,
+    width: wp("32%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("34%"),
+    top: hp("2%"),
+    justifyContent: "flex-start"
+  },
+  Text_I15_601_228_176: {
+    color: "rgba(27, 12, 98, 1)",
+    fontSize: 16,
+    fontWeight: "900",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_I15_601_228_177: {
+    flexGrow: 1,
+    width: wp("5%"),
+    height: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%"),
+    top: hp("2%"),
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    overflow: "hidden"
+  },
+  ImageBackground_I15_601_228_177_154_460: {
+    flexGrow: 1,
+    width: wp("2%"),
+    height: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("1%"),
+    top: hp("0%")
+  },
+  View_15_602: {
+    width: wp("91%"),
+    top: hp("16%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%"),
+    justifyContent: "flex-start"
+  },
+  Text_15_602: {
+    color: "rgba(20, 20, 42, 1)",
+    fontSize: 13,
+    fontWeight: "500",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_15_603: {
+    width: wp("100%"),
+    height: hp("6%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I15_603_137_706: {
+    flexGrow: 1,
+    width: wp("6%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("90%"),
+    top: hp("2%")
+  },
+  View_I15_603_137_707: {
+    width: wp("6%"),
+    minWidth: wp("6%"),
+    height: hp("2%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    borderColor: "rgba(25, 29, 33, 1)",
+    borderWidth: 1
+  },
+  ImageBackground_I15_603_137_708: {
+    width: wp("0%"),
+    minWidth: wp("0%"),
+    height: hp("1%"),
+    minHeight: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("1%")
+  },
+  View_I15_603_137_709: {
+    width: wp("5%"),
+    minWidth: wp("5%"),
+    height: hp("1%"),
+    minHeight: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("1%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(20, 20, 42, 1)"
+  },
+  ImageBackground_I15_603_137_710: {
+    flexGrow: 1,
+    width: wp("4%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("84%"),
+    top: hp("2%")
+  },
+  ImageBackground_I15_603_137_714: {
+    flexGrow: 1,
+    width: wp("5%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("78%"),
+    top: hp("2%")
+  },
+  View_I15_603_137_719: {
+    flexGrow: 1,
+    width: wp("14%"),
+    height: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("1%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I15_603_137_720: {
+    width: wp("14%"),
+    minWidth: wp("14%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("1%"),
+    justifyContent: "flex-start"
+  },
+  Text_I15_603_137_720: {
+    color: "rgba(20, 20, 42, 1)",
+    fontSize: 12,
+    fontWeight: "900",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: -0.30000001192092896,
+    textTransform: "none"
+  },
+  View_15_604: {
+    width: wp("100%"),
+    height: hp("5%"),
+    top: hp("106%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    backgroundColor: "rgba(255, 255, 255, 1)"
+  },
+  View_I15_604_137_722: {
+    flexGrow: 1,
+    width: wp("36%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("32%"),
+    top: hp("3%"),
+    backgroundColor: "rgba(20, 20, 42, 1)"
+  }
+})
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
